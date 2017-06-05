d41e9585578780323bcb7ae34def21b945603139
User <User@User-PC>
2017-06-04 19:29:48 -0600

    New project files, with file implementing "Hello World" program.

--- src/edu/cnm/deepdive/helloworld/HelloWorld.java
+++ src/edu/cnm/deepdive/helloworld/HelloWorld.java
@@ -1,7 +1,14 @@
 package edu.cnm.deepdive.helloworld;
-
+/**
+ * Simple class that displays the "Hello World!!!" message.
+ * @author User
+ */
 public class HelloWorld {
-
+/**
+ * Prints "Hello World!!!" to standard output.
+ * 
+ * @param args    Command-line parameters (ignored)
+ */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
