1835d4ec7a6ac891bb101dfbb26c1695462bdd49
User <User@User-PC>
2017-06-12 09:47:20 -0600

    Unnecessary decomposition (because Nick said so).

--- src/edu/cnm/deepdive/helloworld/HelloWorld.java
+++ src/edu/cnm/deepdive/helloworld/HelloWorld.java
@@ -11,7 +11,11 @@ public class HelloWorld {
  */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		emitMessage("Hello World!!!");
 	}
 
+
+private static void emitMessage(String message) { 
+  System.out.println(message);
+}
 }
